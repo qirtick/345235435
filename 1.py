04389c2c341d0c27360fcae463436040b028d679
MikhailAgafonov <144214080+qirtick@users.noreply.github.com>
2023-10-02 13:54:01 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,75 @@
+# x = int(input('1 число:    '))
+# print(x, x + 1, x + 2, sep='\n')
+
+
+# a = ing(input('длина ребра:  '))
+# v = a ** 3
+# S = 6* a ** 2
+# print(f'Объем = {v}')
+# print('Площадь полной поверхности =', S)
+
+
+# a_1 = int(input('1 член прогрессии '))
+# d = int(input('d '))
+# n = int(input('n '))
+# print(a_1 + d * (n-1))
+
+
+# a = int(input())
+# b = int(input())
+# print(3 * (a + b) * (a + b) * (a + b) + 275 * b * b - 127 * a - 41)
+# score = int(input())
+# print('Следующее за числом', score, 'число:', score + 1)
+# print('Для числа', score, 'предыдущее число:', score - 1)
+# x = int(input())
+# y = int(input())
+# z = int(input())
+# c = int(input())
+# print(3 * (x + y + z + c))
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# x = a + b * (c - 1)
+# print(x)
+# x = int(input())
+# print(x, 2 * x, 3 * x, 4 * x, 5 * x, sep='---')  # 9
+# a = int(input())
+# b = int(input())
+# print(b // a)
+# print(b % a)
+# n = int(input())
+# print(n // 2 + n % 2)  # 11
+# m = int(input())
+# h = m // 60
+# s = m % 60
+# print(m, "мин - это", h, "час", s, "минут.")
+
+
+# num = int(input())
+# a = num % 10
+# b = (num % 100) // 10
+# c = num // 100
+# print("Сумма цифр =", c + b + a)
+# print("Произведение цифр =", c * b * a)
+
+
+# n = int(input())
+# print('   _~_    ' * n)
+# print('  (o o)   ' * n)
+# print(' /  V  \  ' * n)
+# print('/(  _  )\ ' * n)
+# print('  ^^ ^^   ' * n)
+
+
+# abc = int(input())
+# k = 1
+# n = (abc // 10 ** k) % 10
+# print(n)  # 15
+# n = int(input())
+# hours = n % (60 * 24) // 60
+# minutes = n % 60
+# print(hours, minutes)
+
+# n = int(input())
+# print(n // 2 * 2 + 2)
+
